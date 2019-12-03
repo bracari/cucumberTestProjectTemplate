28f8fcd2ed55d5aae131215ffb156de1d4113549
gdWork <gdwork@gdworks-mbp.attlocal.net>
2019-12-03 16:16:07 -0500

    Fixed project errors that prevented test running

--- hellocucumber/target/test-classes/hellocucumber/Stepdefs.java
+++ hellocucumber/target/test-classes/hellocucumber/Stepdefs.java
@@ -1,13 +1,13 @@
 package hellocucumber;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
-import cucumber.api.java.en.Then;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 import static org.junit.Assert.*;
 
-class DoesItPass {
+class IsItFriday {
     static String isItFriday(String today) {
-        return "Nope";
+        return null;
     }
 }
 
@@ -15,17 +15,17 @@ public class Stepdefs {
     private String today;
     private String actualAnswer;
 
-    @Given("^today is Sunday$")
+    @Given("today is Sunday")
     public void today_is_Sunday() {
-        this.today = "Sunday";
+        today = "Sunday";
     }
 
-    @When("^I ask whether it's Friday yet$")
-    public void i_ask_whether_is_s_Friday_yet() {
-        this.actualAnswer = IsItFriday.isItFriday(today);
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
-    @Then("^I should be told \"([^\"]*)\"$")
+    @Then("I should be told {string}")
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
     }
